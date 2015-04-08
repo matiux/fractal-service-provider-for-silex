a910700af3d475b5885f966d5e186dfb90c72848
matiux <m.galacci@gmail.com>
2015-04-09 00:40:21 +0200

    svoluppo provider

--- src/Iuxmat/Silex/Provider/Fractal/FractalServiceProvider.php
+++ src/Iuxmat/Silex/Provider/Fractal/FractalServiceProvider.php
@@ -1,5 +1,33 @@
-<?php 
+<?php namespace Iuxmat\Silex\Provider\Fractal;
 
-class FractalServiceProvider
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use League\Fractal\Manager;
+
+class FractalServiceProvider implements ServiceProviderInterface
 {
+    /**
+     * Registers services on the given app.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     */
+    public function register(Application $app)
+    {
+        $app['fractal'] = $app->share(function () {
+            return new Manager();
+        });
+    }
+
+    /**
+     * Bootstraps the application.
+     *
+     * This method is called after all services are registered
+     * and should be used for "dynamic" configuration (whenever
+     * a service must be requested).
+     */
+    public function boot(Application $app)
+    {
+
+    }
 }
